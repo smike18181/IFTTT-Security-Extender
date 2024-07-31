49e8689e074844edfee0a2012c406981a1638e35
Michele <m.pesce13@studenti.unisa.it>
2024-07-31 16:54:53 +0200

    build(backend): add unversioned files

---
+++ IFTTT Security Extension App/DatabaseLoader/src/main/java/com/iftttse/dbloader/DatabaseLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.iftttse.dbloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseLoaderApplication.class, args);
+	}
+
+}
---
+++ IFTTT Security Extension App/LinkLoader/src/main/java/com/iftttse/linkloader/LinkLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.iftttse.linkloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkLoaderApplication.class, args);
+    }
+
+}
